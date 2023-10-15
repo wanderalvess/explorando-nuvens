6f13fff95e3ee4cd6e359d82d254c89bb19bf5e2
Wanderson Alves Santos <wandersonalves.s@hotmail.com>
2023-10-14 23:01:50 -0300

    melhorias em geral

--- src/main/java/com/wanderalvess/cloudsidentificationtabback/ImageController.java
+++ src/main/java/com/wanderalvess/cloudsidentificationtabback/ImageController.java
@@ -1,22 +1,21 @@
 package com.wanderalvess.cloudsidentificationtabback;
 
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.core.io.ByteArrayResource;
-import org.springframework.core.io.Resource;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:56032")
 public class ImageController {
 
     @GetMapping(value = "/image", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -58,22 +57,35 @@ public class ImageController {
     @GetMapping(value = "/images", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<Image>> getImages(@RequestParam("category") String category) {
         List<Image> images = new ArrayList<>();
-
-        if ("cirrus".equalsIgnoreCase(category)) {
+        System.out.println(category);
+        if ("Nuvens Cirrus".equalsIgnoreCase(category)) {
             images.add(new Image("cirrus1", "https://i.postimg.cc/C54KNSmC/MG-3831.jpg"));
             images.add(new Image("cirrus2", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
             images.add(new Image("cirrus3", "https://i.postimg.cc/4yvrH3BG/MG-4210.jpg"));
             images.add(new Image("cirrus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
-        } else if ("cumulus".equalsIgnoreCase(category)) {
+            images.add(new Image("cirrus1", "https://i.postimg.cc/C54KNSmC/MG-3831.jpg"));
+            images.add(new Image("cirrus2", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
+            images.add(new Image("cirrus3", "https://i.postimg.cc/4yvrH3BG/MG-4210.jpg"));
+            images.add(new Image("cirrus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+
+        } else if ("Nuvens Cumulus".equalsIgnoreCase(category)) {
             images.add(new Image("cumulus1", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
-//            images.add(new Image("cumulus2", "C:\\Users\\wande\\Projetos\\clouds-identification-tab-back\\cumulus.JPG"));
-//            images.add(new Image("cumulus3", "C:\\Users\\wande\\Projetos\\clouds-identification-tab-back\\cumulus.JPG"));
-//            images.add(new Image("cumulus4", "C:\\Users\\wande\\Projetos\\clouds-identification-tab-back\\cumulus.JPG"));
-        } else if ("stratus".equalsIgnoreCase(category)) {
+            images.add(new Image("cumulus2", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("cumulus3", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("cumulus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("cumulus1", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
+            images.add(new Image("cumulus2", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("cumulus3", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("cumulus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+        } else if ("Nuvens Stratus".equalsIgnoreCase(category)) {
             images.add(new Image("stratus1", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
             images.add(new Image("stratus2", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
-    //            images.add(new Image("cumulus3", "C:\\Users\\wande\\Projetos\\clouds-identification-tab-back\\cumulus.JPG"));
-    //            images.add(new Image("cumulus4", "C:\\Users\\wande\\Projetos\\clouds-identification-tab-back\\cumulus.JPG"));
+            images.add(new Image("stratus3", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("stratus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("stratus1", "https://i.postimg.cc/VNP3YnY6/MG-3829.jpg"));
+            images.add(new Image("stratus2", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("stratus3", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
+            images.add(new Image("stratus4", "https://i.postimg.cc/Vk2D18WW/MG-4226.jpg"));
         } else {
             // Lógica para tratar categorias desconhecidas ou erro
             // Neste exemplo, estou retornando uma lista vazia para categorias desconhecidas.
@@ -82,3 +94,4 @@ public class ImageController {
         return ResponseEntity.ok(images);
     }
 }
+
